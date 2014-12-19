53a19d751e1050018f057f3c2492c86a03c1e2d4
alumno <alumno@Mac-Pro-de-alumno.local>
2014-12-19 02:44:42 -0800

    Implementación del segue de 'Edit'

--- Agenda/DetalleViewController.swift
+++ Agenda/DetalleViewController.swift
@@ -38,14 +38,18 @@ class DetalleViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "Edit"{
+            let editViewControler = segue.destinationViewController as EditViewController
+            editViewControler.person = person!
+        }
     }
-    */
+    
 
 }
